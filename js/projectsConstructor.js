5cd6c4521830e5e0b4d73a824bdf67482f25f1d2
austriker27 <davidalindahl@gmail.com>
2017-09-11 16:40:17 -0700

    added constructor for new projects

---
+++ js/projectsConstructor.js
@@ -0,0 +1,11 @@
+'use strict';
+
+function Project (name, url, github, img, codebases) {
+  this.name = name;
+  this.url = url;
+  this.github = github;
+  this.img = img;
+  this.codebases = codebases;
+}
+
+new Project ('Ghostown', 'http://ghostowngame.com', 'http://github.com/austriker27/ghostowngame', '../imgs/ghostowngame.jpg', 'Javascript, CSS & HTML');
