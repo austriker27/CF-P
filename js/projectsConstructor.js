bba204bd9cc89e3403dffa234ba4dea7f89b8fe4
austriker27 <davidalindahl@gmail.com>
2017-09-11 18:31:58 -0700

    constructor and JS for menu

--- js/projectsConstructor.js
+++ js/projectsConstructor.js
@@ -6,11 +6,39 @@ function Project (name, url, github, img, codebases) {
   this.github = github;
   this.img = img;
   this.codebases = codebases;
+  this.renderProject = function () {
+    var anchor = document.getElementById('Portfolio');
+    var projectName = document.createElement('h3');
+    projectName.innerText = this.name;
+    anchor.appendChild(projectName);
+
+    var projectUrl = document.createElement('a');
+    projectUrl.innerText = this.url;
+    projectName.appendChild(projectUrl);
+
+    var projectRepo = document.createElement('a');
+    projectRepo.innerText = this.github;
+    projectName.appendChild(projectRepo);
+
+    var projectImg = document.createElement('img');
+    projectImg.innerHTML = this.img;
+    projectName.appendChild(projectImg);
+
+    var projectLanguages = document.createElement('p');
+    projectLanguages.innerText = this.codebases;
+    projectName.appendChild(projectLanguages);
+  }
 }
 
-new Project ('Ghostown', 'http://ghostowngame.com', 'http://github.com/austriker27/ghostowngame', '../imgs/ghostowngame.jpg', 'Javascript, CSS & HTML');
-
+// new instances of projects
 // projects to add : stop32, rainier watch, seattle adventure club, corn beans betas, david lindahl photography
 
+var ghostown = new Project('Ghostown', 'http://ghostowngame.com', 'http://github.com/austriker27/ghostowngame', '../imgs/ghostowngame.jpg', 'Javascript, CSS & HTML');
 
-// add function to create projects to project section
+// event listener for hamburger menu and function to toggle hamburger menu on and off
+var hamburgerMenu = document.getElementById('hamburger');
+hamburgerMenu.addEventListener('click', toggleHamburger);
+
+function toggleHamburger () {
+
+}
