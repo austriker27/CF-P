cfcb68fab1168f5e8c223039e703b5ad6bb586e5
austriker27 <davidalindahl@gmail.com>
2017-09-11 18:12:28 -0700

    fixed top nav bar to be mobile first

--- js/projectsConstructor.js
+++ js/projectsConstructor.js
@@ -9,3 +9,8 @@ function Project (name, url, github, img, codebases) {
 }
 
 new Project ('Ghostown', 'http://ghostowngame.com', 'http://github.com/austriker27/ghostowngame', '../imgs/ghostowngame.jpg', 'Javascript, CSS & HTML');
+
+// projects to add : stop32, rainier watch, seattle adventure club, corn beans betas, david lindahl photography
+
+
+// add function to create projects to project section
