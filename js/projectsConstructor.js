9251cd81a2de692b001ae3dc5e0c2ed89b52aef5
austriker27 <davidalindahl@gmail.com>
2017-09-12 16:38:41 -0700

    added initial scale to the meta tag

--- js/projectsConstructor.js
+++ js/projectsConstructor.js
@@ -6,31 +6,32 @@ function Project (name, url, github, img, codebases) {
   this.github = github;
   this.img = img;
   this.codebases = codebases;
-  this.renderProject = function () {
-    var anchor = document.getElementById('Portfolio');
-    var projectName = document.createElement('h3');
-    projectName.innerText = this.name;
-    anchor.appendChild(projectName);
-
-    var projectUrl = document.createElement('a');
-    projectUrl.innerText = this.url;
-    projectName.appendChild(projectUrl);
-
-    var projectRepo = document.createElement('a');
-    projectRepo.innerText = this.github;
-    projectName.appendChild(projectRepo);
-
-    var projectImg = document.createElement('img');
-    projectImg.innerHTML = this.img;
-    projectName.appendChild(projectImg);
-
-    var projectLanguages = document.createElement('p');
-    projectLanguages.innerText = this.codebases;
-    projectName.appendChild(projectLanguages);
-  }
 }
 
-// TODO projects to add : stop32, rainier watch, seattle adventure club, corn beans betas, david lindahl photography
+this.prototype.renderProject = function () {
+  var anchor = document.getElementById('Portfolio');
+  var projectName = document.createElement('h3');
+  projectName.innerText = this.name;
+  anchor.appendChild(projectName);
+
+  var projectUrl = document.createElement('a');
+  projectUrl.innerText = this.url;
+  projectName.appendChild(projectUrl);
+
+  var projectRepo = document.createElement('a');
+  projectRepo.innerText = this.github;
+  projectName.appendChild(projectRepo);
+
+  var projectImg = document.createElement('img');
+  projectImg.innerHTML = this.img;
+  projectName.appendChild(projectImg);
+
+  var projectLanguages = document.createElement('p');
+  projectLanguages.innerText = this.codebases;
+  projectName.appendChild(projectLanguages);
+}
+
+TODO projects to add : stop32, rainier watch, seattle adventure club, corn beans betas, david lindahl photography
 
 var ghostown = new Project('Ghostown', 'http://ghostowngame.com', 'http://github.com/austriker27/ghostowngame', '../imgs/ghostowngame.jpg', 'Javascript, CSS & HTML');
 
