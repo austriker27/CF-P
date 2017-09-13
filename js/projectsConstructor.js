160ba2644a99b1f6435269dadf2c06cc837d754c
austriker27 <davidalindahl@gmail.com>
2017-09-12 20:38:49 -0700

    changes to the js constructor and i cant really remember what specifically happened

--- js/projectsConstructor.js
+++ js/projectsConstructor.js
@@ -12,6 +12,16 @@ function Project (name, url, github, img, codebases) {
 
 var ghostown = new Project('Ghostown', 'http://ghostowngame.com', 'http://github.com/austriker27/ghostowngame', '../imgs/ghostowngame.jpg', 'Javascript, CSS & HTML');
 
+var stop32 = new Project('STOP32 Photography Club', 'http://stop32.org','https://github.com/austriker27/stop32.org', '../imgs/stop32.jpg', 'HTML & CSS');
+
+var RainierWatch = new Project('Mount Rainier Watch', 'http://rainierwatch.com','https://github.com/austriker27/rainierwatch', '../imgs/rainierwatch.jpg', 'Javascript, HTML & CSS');
+
+var SeattleAdventureClub = new Project('Seattle Adventure Club', 'http://seattleadventureclub.org','https://github.com/austriker27/seattleadventureclub', '../imgs/seattleadventureclub.jpg', 'HTML & CSS');
+
+var CornBeansBetas = new Project('Corn, Beans, & Betas', 'http://cornbeansbetas.com','#', '../imgs/cornbeansbetas.jpg', 'WordPress, HTML & CSS');
+
+var DavidLindahlPhoto = new Project('David Lindahl Photography', 'http://davidlindahlphoto.com','#', '../imgs/davidlindahlphoto.jpg', 'Design, & Photography');
+
 Project.prototype.renderProject = function () {
   var $newProject = $('template').clone();
   $newProject.find('h3')
