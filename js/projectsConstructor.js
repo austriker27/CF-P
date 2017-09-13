d197c7579e6434a0330d92700f844eb542db4aa7
austriker27 <davidalindahl@gmail.com>
2017-09-12 18:08:19 -0700

    corrected icons to be linked to external sites in new windows

--- js/projectsConstructor.js
+++ js/projectsConstructor.js
@@ -8,29 +8,20 @@ function Project (name, url, github, img, codebases) {
   this.codebases = codebases;
 }
 
-this.prototype.renderProject = function () {
-  var anchor = document.getElementById('Portfolio');
-  var projectName = document.createElement('h3');
-  projectName.innerText = this.name;
-  anchor.appendChild(projectName);
-
-  var projectUrl = document.createElement('a');
-  projectUrl.innerText = this.url;
-  projectName.appendChild(projectUrl);
-
-  var projectRepo = document.createElement('a');
-  projectRepo.innerText = this.github;
-  projectName.appendChild(projectRepo);
-
-  var projectImg = document.createElement('img');
-  projectImg.innerHTML = this.img;
-  projectName.appendChild(projectImg);
-
-  var projectLanguages = document.createElement('p');
-  projectLanguages.innerText = this.codebases;
-  projectName.appendChild(projectLanguages);
-}
-
 // TODO: projects to add : stop32, rainier watch, seattle adventure club, corn beans betas, david lindahl photography
 
 var ghostown = new Project('Ghostown', 'http://ghostowngame.com', 'http://github.com/austriker27/ghostowngame', '../imgs/ghostowngame.jpg', 'Javascript, CSS & HTML');
+
+Project.prototype.renderProject = function () {
+  var $newProject = $('template').clone();
+  $newProject.find('h3')
+             .text(this.name);
+  $newProject.find('a')
+             .eq(1).attr('href', 'this.url');
+  $newProject.find('a')
+             .eq(2).attr('href','this.github');
+  $newProject.find('img')
+             .eq(2).src('this.img');
+  $newProject.find('p')
+             .eq(1).text('this.codebases');
+}
