fd456631e58d342ede1f619d4090d0d87b59eeba
austriker27 <davidalindahl@gmail.com>
2017-09-13 12:10:22 -0700

    refactoring constructors based on todays lab

---
+++ js/projects.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var projectsList = [
+  {
+    title: 'Ghostown',
+    url: 'http://ghostowngame.com',
+    githubURL:'http://github.com/austriker27/ghostowngame',
+    img: '../imgs/ghostowngame.jpg',
+      skillsUsed: 'Javascript, CSS & HTML',
+  },
+  {
+    title: 'STOP32 Photography Club',
+    url:'http://stop32.org',
+    githubURL: 'https://github.com/austriker27/stop32.org',
+    img: '../imgs/stop32.jpg',
+      skillsUsed: 'HTML & CSS',
+  },
+  {
+    title: 'Mount Rainier Watch',
+    url: 'http://rainierwatch.com'
+    githubURL: 'https://github.com/austriker27/rainierwatch'
+    img: '../imgs/rainierwatch.jpg',
+      skillsUsed: 'Javascript, HTML & CSS',
+  },
+  {
+    title: 'Seattle Adventure Club',
+    url: 'http://seattleadventureclub.org',
+    githubURL: 'https://github.com/austriker27/seattleadventureclub',
+    img: '../imgs/seattleadventureclub.jpg',
+      skillsUsed: 'HTML & CSS',
+  },
+  {
+    title: 'Corn, Beans, & Betas Blog',
+    url: 'http://cornbeansbetas.com',
+    githubURL: '#',
+    img: '../imgs/cornbeansbetas.jpg',
+    skillsUsed: 'WordPress, HTML & CSS',
+  }
+]
--- js/projectsConstructor.js
+++ js/projectsConstructor.js
@@ -1,29 +1,16 @@
 'use strict';
 
-function Project (name, url, github, img, codebases) {
-  this.name = name;
-  this.url = url;
-  this.github = github;
-  this.img = img;
-  this.codebases = codebases;
+function Project (projectsListObj) {
+  this.title = projectsListObj.title;
+  this.url = projectsListObj.url;
+  this.githubURL = projectsListObj.githubURL;
+  this.img = projectsListObj.img;
+  this.skillsUsed = projectsListObj.skillsUsed;
   Project.all.push(this);
-
 }
 
-var ghostown = new Project('Ghostown', 'http://ghostowngame.com', 'http://github.com/austriker27/ghostowngame', '../imgs/ghostowngame.jpg', 'Javascript, CSS & HTML');
-
-var stop32 = new Project('STOP32 Photography Club', 'http://stop32.org','https://github.com/austriker27/stop32.org', '../imgs/stop32.jpg', 'HTML & CSS');
-
-var RainierWatch = new Project('Mount Rainier Watch', 'http://rainierwatch.com','https://github.com/austriker27/rainierwatch', '../imgs/rainierwatch.jpg', 'Javascript, HTML & CSS');
-
-var SeattleAdventureClub = new Project('Seattle Adventure Club', 'http://seattleadventureclub.org','https://github.com/austriker27/seattleadventureclub', '../imgs/seattleadventureclub.jpg', 'HTML & CSS');
-
-var CornBeansBetas = new Project('Corn, Beans, & Betas', 'http://cornbeansbetas.com','#', '../imgs/cornbeansbetas.jpg', 'WordPress, HTML & CSS');
-
-var DavidLindahlPhoto = new Project('David Lindahl Photography', 'http://davidlindahlphoto.com','#', '../imgs/davidlindahlphoto.jpg', 'Design, & Photography');
-
 Project.prototype.renderProject = function() {
-  var $newProject = $('template').clone();
+  var $newProject = $('.template').clone();
   $newProject.find('h3')
              .text(this.name);
   $newProject.find('a')
@@ -35,3 +22,21 @@ Project.prototype.renderProject = function() {
   $newProject.find('p')
              .eq(1).text('this.codebases');
 }
+
+Project.all = [];
+
+
+Project.initializeTasks = function(){
+  projects.forEach(taskObj => new Project(taskObj.name, taskObj.desc, taskObj.img, taskObj.url));
+  listTask();
+}
+
+function listTask(){
+  Project.all.forEach(task => task.render());
+}
+
+Project.initializeTasks();
+
+$('.hamburger').on('click', function() {
+  $('.hide').toggle('.nav');
+});
