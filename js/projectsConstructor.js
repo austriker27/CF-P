1cf3d078ebfe07e8e76f939a5db10132c625bd8a
austriker27 <davidalindahl@gmail.com>
2017-09-13 12:47:42 -0700

    did a bunch of stuff that didnt work and the constructor remains broken.

--- js/projectsConstructor.js
+++ js/projectsConstructor.js
@@ -11,32 +11,20 @@ function Project (projectsListObj) {
 
 Project.prototype.renderProject = function() {
   var $newProject = $('.template').clone();
+  $newProject.removeClass('template');
   $newProject.find('h3')
              .text(this.name);
   $newProject.find('a')
              .eq(1).attr('href', 'this.url');
   $newProject.find('a')
-             .eq(2).attr('href','this.github');
+             .eq(2).attr('href','this.githubURL');
   $newProject.find('img')
              .eq(2).src('this.img');
   $newProject.find('p')
-             .eq(1).text('this.codebases');
+             .eq(1).text('this.skillsUsed');
+
+  $('.template').append($newProject);
+
 }
 
 Project.all = [];
-
-
-Project.initializeTasks = function(){
-  projects.forEach(taskObj => new Project(taskObj.name, taskObj.desc, taskObj.img, taskObj.url));
-  listTask();
-}
-
-function listTask(){
-  Project.all.forEach(task => task.render());
-}
-
-Project.initializeTasks();
-
-$('.hamburger').on('click', function() {
-  $('.hide').toggle('.nav');
-});
