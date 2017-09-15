fd76d49bcf48fef948bd8d2d18cfdba673b6f08b
austriker27 <davidalindahl@gmail.com>
2017-09-15 14:26:50 -0700

    moved js files to be more semantic

--- js/projectsConstructor.js
+++ js/projectsConstructor.js
@@ -24,39 +24,3 @@ projectsList.forEach(function(projectObject) {
 projects.forEach(function(project){
   $('.projectsAnchor').append(project.toHtml());
 });
-
-// this code makes you scroll to the top of section
-$('#aboutMe').on('click', function(){
-  $('main').hide();
-  $('main [id|="'+$(this).attr('data-content')+'"]').fadeIn();
-  $('html, body').animate({
-    scrollTop: $('#bannerText').position().top
-  });
-});
-
-$('#mySkills').on('click', function(){
-  $('html, body').animate({
-    scrollTop: $('#mySkillsSection').position().top
-  });
-});
-
-$('#myProjects').on('click', function(){
-  $('html, body').animate({
-    scrollTop: $('#Portfolio').position().top
-  });
-});
-
-Project.makeSectionsAppear = function() {
-  $('.topNav').on('click', '.navTab', function() {
-    $('.sectionContent').hide();
-    $('#' + $(this).attr('data-content')).fadeIn();
-    console.log($('#'+$(this).attr('data-content' + ' ' )));
-  });
-
-  // $('.topNav .navTab:first').click();
-};
-
-
-$(document).ready(function() {
-  Project.makeSectionsAppear();
-});
