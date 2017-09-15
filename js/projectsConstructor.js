30ed07d7bbedb3517fd94238491511657794bf48
austriker27 <davidalindahl@gmail.com>
2017-09-15 12:08:44 -0700

    uncommented a lot of code for handing in

--- js/projectsConstructor.js
+++ js/projectsConstructor.js
@@ -8,9 +8,7 @@ function Project (projectsListObj) {
   this.githubURL = projectsListObj.githubURL;
   this.img = projectsListObj.img;
   this.skillsUsed = projectsListObj.skillsUsed;
-  // Project.all.push(this);
 }
-// Project.all = [];
 
 Project.prototype.toHtml = function() {
   var sourceHTML = $('#project-template').html();
@@ -24,41 +22,41 @@ projectsList.forEach(function(projectObject) {
 });
 
 projects.forEach(function(project){
-  $('.projects').append(project.toHtml());
+  $('.projectsAnchor').append(project.toHtml());
 });
 
-// // this code makes you scroll to the top of section
-// $('#aboutMe').on('click', function(){
-//   $('main').hide();
-//   $('main [id|="'+$(this).attr('data-content')+'"]').fadeIn();
-//   $('html, body').animate({
-//     scrollTop: $('#bannerText').position().top
-//   });
-// });
-//
-// $('#mySkills').on('click', function(){
-//   $('html, body').animate({
-//     scrollTop: $('#mySkillsSection').position().top
-//   });
-// });
-//
-// $('#myProjects').on('click', function(){
-//   $('html, body').animate({
-//     scrollTop: $('#Portfolio').position().top
-//   });
-// });
-//
-// Project.makeSectionsAppear = function() {
-//   $('.topNav').on('click', '.navTab', function() {
-//     $('.sectionContent').hide();
-//     $('#'+$(this).attr('data-content')+' ' ).fadeIn();
-//     console.log($('#'+$(this).attr('data-content' + ' ' )));
-//   });
-//
-//   // $('.topNav .navTab:first').click();
-// };
-//
-//
-// $(document).ready(function() {
-//   Project.makeSectionsAppear();
-// });
+// this code makes you scroll to the top of section
+$('#aboutMe').on('click', function(){
+  $('main').hide();
+  $('main [id|="'+$(this).attr('data-content')+'"]').fadeIn();
+  $('html, body').animate({
+    scrollTop: $('#bannerText').position().top
+  });
+});
+
+$('#mySkills').on('click', function(){
+  $('html, body').animate({
+    scrollTop: $('#mySkillsSection').position().top
+  });
+});
+
+$('#myProjects').on('click', function(){
+  $('html, body').animate({
+    scrollTop: $('#Portfolio').position().top
+  });
+});
+
+Project.makeSectionsAppear = function() {
+  $('.topNav').on('click', '.navTab', function() {
+    $('.sectionContent').hide();
+    $('#' + $(this).attr('data-content')).fadeIn();
+    console.log($('#'+$(this).attr('data-content' + ' ' )));
+  });
+
+  $('.topNav .navTab:first').click();
+};
+
+
+$(document).ready(function() {
+  Project.makeSectionsAppear();
+});
