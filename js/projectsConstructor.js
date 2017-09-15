80a095f874921bc1952ab44cf4b36f0a36dc2133
austriker27 <davidalindahl@gmail.com>
2017-09-14 20:30:50 -0700

    setup handlebars template and hiding all content areas

--- js/projectsConstructor.js
+++ js/projectsConstructor.js
@@ -10,19 +10,23 @@ function Project (projectsListObj) {
 }
 
 Project.prototype.toHtml = function() {
-  var $newProject = $('.template').clone();
-  $newProject.removeClass('template');
-  $newProject.find('h3')
-             .text(this.title);
-  $newProject.find('a')
-             .eq(0).attr('href', this.url);
-  $newProject.find('a')
-             .eq(1).attr('href',this.githubURL);
-  $newProject.find('img')
-             .attr('src',this.img);
-  $newProject.find('p')
-             .eq(0).text(this.skillsUsed);
-  $('.anchorProjects').append($newProject);
+  var sourceHTML = $('#article-template').html();
+  var actualTemplate = Handlebars.compile(sourceHTML);
+  // var $newProject = $('.template').clone();
+  // $newProject.removeClass('template');
+  // $newProject.find('h3')
+  //            .text(this.title);
+  // $newProject.find('a')
+  //            .eq(0).attr('href', this.url);
+  // $newProject.find('a')
+  //            .eq(1).attr('href',this.githubURL);
+  // $newProject.find('img')
+  //            .attr('src',this.img);
+  // $newProject.find('p')
+  //            .eq(0).text(this.skillsUsed);
+  // $('.anchorProjects').append($newProject);
+  var newRawHTML = actualTemplate(this);
+  $('main').append(newRawHTML);
 };
 
 Project.all = [];
@@ -34,23 +38,37 @@ projectsList.forEach(function(project) {
 Project.all.forEach(function(project) {
   project.toHtml();
 });
+// this code makes you scroll to the top of section
+// $('#aboutMe').on('click', function(){
+//   $('main').hide();
+//   $('main [id|="'+$(this).attr('data-content')+'"]').fadeIn();
+//   $('html, body').animate({
+//     scrollTop: $('#bannerText').position().top
+//   });
+// });
+//
+// $('#mySkills').on('click', function(){
+//   $('html, body').animate({
+//     scrollTop: $('#mySkillsSection').position().top
+//   });
+// });
+//
+// $('#myProjects').on('click', function(){
+//   $('html, body').animate({
+//     scrollTop: $('#Portfolio').position().top
+//   });
+// });
 
-$('#aboutMe').on('click', function(){
-  $('main').hide();
-  $('main [id|="'+$(this).attr('data-content')+'"]').fadeIn();
-  $('html, body').animate({
-    scrollTop: $('#bannerText').position().top
+Project.all.makeSectionsAppear = function() {
+  $('.topNav').on('click', '.navTab', function() {
+    $('.sectionContent').hide();
+    $('#' + $(this).data('content')).fadeIn();
   });
-});
 
-$('#mySkills').on('click', function(){
-  $('html, body').animate({
-    scrollTop: $('#mySkillsSection').position().top
-  });
-});
+  $('.topNav .navTab:first').click();
+};
 
-$('#myProjects').on('click', function(){
-  $('html, body').animate({
-    scrollTop: $('#Portfolio').position().top
-  });
+
+$(document).ready(function() {
+  Project.all.makeSectionsAppear();
 });
