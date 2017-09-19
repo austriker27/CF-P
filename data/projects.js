45d98b81ef396a50d36c14c8e67d02ee9e6a6c70
austriker27 <davidalindahl@gmail.com>
2017-09-18 19:59:22 -0700

    fixing gh pages again for images to work

--- data/projects.js
+++ data/projects.js
@@ -5,35 +5,35 @@ var projectsList = [
     title: 'Ghostown',
     url: 'http://ghostowngame.com',
     githubURL:'http://github.com/austriker27/ghostowngame',
-    img: '../imgs/ghostown-mockup.jpg',
+    img: '/imgs/ghostown-mockup.jpg',
     skillsUsed: 'Javascript, CSS & HTML',
   },
   {
     title: 'STOP32 Photography Club',
     url:'http://stop32.org',
     githubURL: 'https://github.com/austriker27/stop32.org',
-    img: '../imgs/stop32-mockup.jpg',
+    img: '/imgs/stop32-mockup.jpg',
     skillsUsed: 'HTML & CSS',
   },
   {
     title: 'Mount Rainier Watch',
     url: 'http://rainierwatch.com',
     githubURL: 'https://github.com/austriker27/rainierwatch',
-    img: '../imgs/rainierwatch-mockup.jpg',
+    img: '/imgs/rainierwatch-mockup.jpg',
     skillsUsed: 'Javascript, HTML & CSS',
   },
   {
     title: 'Seattle Adventure Club',
     url: 'http://seattleadventureclub.org',
     githubURL: 'https://github.com/austriker27/seattleadventureclub',
-    img: '../imgs/seattleadventureclub-mockup.jpg',
+    img: '/imgs/seattleadventureclub-mockup.jpg',
     skillsUsed: 'HTML & CSS',
   },
   {
     title: 'Corn, Beans, & Betas Blog',
     url: 'http://cornbeansbetas.com',
     githubURL: '#',
-    img: '../imgs/cornbeansbetas-mockup.jpg',
+    img: '/imgs/cornbeansbetas-mockup.jpg',
     skillsUsed: 'WordPress, HTML & CSS',
   }
-]
+];
