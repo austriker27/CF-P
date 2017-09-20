eed4ed8ce82b9bf0188910138d1d5b32c47fb5d6
austriker27 <davidalindahl@gmail.com>
2017-09-19 21:02:38 -0700

    added server js files and created package json

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const EXPRESS = require('express');
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 3000;
+const APP = EXPRESS();
+
+APP.use(EXPRESS.static('public'));
+
+APP.get('/', function(request, response) {
+  response.sendFile('index.html', {root: './public'});
+});
+
+APP.get('*', function(request, response) {
+  response.sendFile('404.html', {root: './public'});
+});
