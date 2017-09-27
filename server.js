4754b6f98c43201bdc9b3cdd997c6f49fadd3c4c
austriker27 <davidalindahl@gmail.com>
2017-09-27 11:10:36 -0700

    added github proxy functionality

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 const EXPRESS = require('express');
 const bodyParser = require('body-parser');
+const requestProxy = require('express-request-proxy');
 const PORT = process.env.PORT || 3000;
 const APP = EXPRESS();
 
@@ -19,3 +20,14 @@ APP.listen(PORT, function(){
   console.log(PORT);
 }
 );
+
+APP.get('/github/*', proxyGitHub);
+
+function proxyGitHub(req, res, next){
+  (requestProxy({
+    url: `https://api.github.com/${req.params[0]}`,
+    headers: {
+      Authorization: `token ${process.env.GITHUB_TOKEN}`
+    }
+  }))(req, res);
+}
