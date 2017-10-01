30fc2c182223d894f8b6438544f8ac419ac8329f
austriker27 <davidalindahl@gmail.com>
2017-10-01 13:32:22 -0700

    added server files to server js from class lab per instructions

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 'use strict';
 
+const PG = require('pg');
+const FS = require('fs');
 const EXPRESS = require('express');
 const bodyParser = require('body-parser');
 const requestProxy = require('express-request-proxy');
 const PORT = process.env.PORT || 3000;
+const conString = `${process.env.DATABASE_URL}`;
+const client = new PG.Client(conString);
+client.connect();
+client.on('error', err => console.error(err));
+
 const APP = EXPRESS();
 
+APP.use(bodyParser.json());
+APP.use(bodyParser.urlencoded({extended: true}));
 APP.use(EXPRESS.static('public'));
 
 APP.get('/', function(request, response) {
